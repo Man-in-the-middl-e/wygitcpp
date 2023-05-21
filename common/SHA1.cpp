f93a79e31f213b13e0d4017c4a08ba6d7c760480
Man-in-the-middl-e <dlyzno@gmail.com>
2023-05-21 22:59:54 +0200

    Add wrapper around SHA1 boost class, that calculate SHA1 on provided data and convert it to the readable hash string.

---
+++ common/SHA1.cpp
@@ -0,0 +1,39 @@
+#include "SHA1.hpp"
+
+#include <boost/uuid/detail/sha1.hpp>
+#include <sstream>
+
+SHA1::SHA1(const std::string& data)
+    : m_hashData(SHA1ToString(generateSHA(data)))
+{
+}
+const std::string& SHA1::toString() const { return m_hashData; }
+
+std::string SHA1::generateSHA(const std::string& data) const
+{
+    boost::uuids::detail::sha1 s;
+    s.process_bytes(data.c_str(), data.size());
+    unsigned int digest[DIGEST_SIZE_WORD];
+    s.get_digest(digest);
+
+    std::string hash(HASH_SIZE_BYTES, '\0');
+    for (int i = 0; i < DIGEST_SIZE_WORD; ++i) {
+        const char* tmp = reinterpret_cast<char*>(digest);
+        hash[i * 4] = tmp[i * 4 + 3];
+        hash[i * 4 + 1] = tmp[i * 4 + 2];
+        hash[i * 4 + 2] = tmp[i * 4 + 1];
+        hash[i * 4 + 3] = tmp[i * 4];
+    }
+    return hash;
+}
+
+std::string SHA1::SHA1ToString(const std::string& hash) const
+{
+    std::string res(hash.size() * 2, '\0');
+    std::string_view hexDigits = "0123456789abcdef";
+    for (size_t i = 0; i < hash.size(); ++i) {
+        res[i * 2] = hexDigits[(hash[i] >> 4) & 0xf];
+        res[i * 2 + 1] = hexDigits[hash[i] & 0xf];
+    }
+    return res;
+}
---
+++ common/SHA1.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+class SHA1 {
+  private:
+    static constexpr uint8_t HASH_SIZE_BYTES = 20;
+    static constexpr uint8_t DIGEST_SIZE_WORD = 5;
+
+  public:
+    SHA1(const std::string& data);
+    const std::string& toString() const;
+
+  private:
+    std::string generateSHA(const std::string& data) const;
+
+    std::string SHA1ToString(const std::string& hash) const;
+
+  private:
+    std::string m_hashData;
+};
