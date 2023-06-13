c527adfa3452e3036e122fa6e17806e8d90cffb7
Man-in-the-middl-e <dlyzno@gmail.com>
2023-06-13 23:47:31 +0200

    Add test for the hash-file commad.

--- tests/wyagitTests.cpp
+++ tests/wyagitTests.cpp
@@ -1,27 +1,35 @@
-#include <gtest/gtest.h>
 #include <boost/property_tree/ini_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include <gtest/gtest.h>
 
 #include "../CommandArguments.hpp"
 
-std::filesystem::path REPO_PATH = "gitTest";
-class TestInitCleanup {
-  public:
-    TestInitCleanup() {}
+std::filesystem::path REPO_PATH = std::filesystem::current_path() / "gitTest";
 
-    ~TestInitCleanup() { cleanRoot(); }
+class GitCommandsTest : public ::testing::Test {
+  public:
+
+  protected:
+    void SetUp() override
+    {
+        GitCommands::init(REPO_PATH);
+    }
+
+    void TearDown() override
+    {
+        cleanRoot();
+    }
 
   private:
     void cleanRoot() { std::filesystem::remove_all(REPO_PATH); }
 };
 
-TEST(GitCommands, GitInit)
+TEST_F(GitCommandsTest, GitInit)
 {
-    GitCommands::init(REPO_PATH);
     EXPECT_TRUE(std::filesystem::exists(REPO_PATH));
     EXPECT_TRUE(std::filesystem::exists(REPO_PATH / ".git"));
 
-    auto pathToConfig= REPO_PATH / ".git" / "config";
+    auto pathToConfig = REPO_PATH / ".git" / "config";
     EXPECT_TRUE(std::filesystem::exists(pathToConfig));
 
     boost::property_tree::ptree config;
@@ -29,9 +37,17 @@ TEST(GitCommands, GitInit)
     ASSERT_EQ(config.get<int>("core.repositoryformatversion"), 0);
 }
 
+TEST_F(GitCommandsTest, HashFileBlob)
+{
+    auto textFile = REPO_PATH / "test.txt";
+    Utilities::writeToFile(textFile, "text");
+    auto fileHash = GitCommands::hashFile(textFile, "blob");
+    auto fileHashFromFS = GitObject::findObject(fileHash.data(), "blob");
+    EXPECT_EQ(fileHash.data(), fileHashFromFS.data());
+}
+
 int main(int argc, char* argv[])
 {
-    TestInitCleanup test;
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
